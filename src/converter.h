2ea680cd99fae04dcbbaef947477ba98306313d6
burger4d <b4d@gmail.com>
2026-03-21 02:13:02 +0100

    update for  tests

--- src/converter.h
+++ src/converter.h
@@ -4,16 +4,19 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LOOP_STACK_SIZE 1024
+#define LOOP_SIZE 32
+
 struct loop_ids
 {
     int loop_id;
-    int loop_stack[1024];
+    int loop_stack[LOOP_STACK_SIZE];
     int sp;
 
-    char loop_start[32];
-    char exec_start[32];
-    char loop_end[32];
-    char exec_end[32];
+    char loop_start[LOOP_SIZE];
+    char exec_start[LOOP_SIZE];
+    char loop_end[LOOP_SIZE];
+    char exec_end[LOOP_SIZE];
 };
 
 char *convert(char *bf_code, char *buf);
--- tests/test_.py
+++ tests/test_.py
@@ -2,18 +2,20 @@ import pytest
 import brainfuck as bf
 import subprocess as sp
 
+timeout = 2
+
 def run_code(code):
-    sp.run(["rm", "a.out", "output.S"])
+    sp.run(["rm", "-f", "a.out", "output.S"])
     sp.run(["./bf", "-c", code])
     sp.run(["gcc", "check.c", "output.S"])
-    result = sp.run(["./a.out"], stdout=sp.PIPE)
-    return result.stdout.decode("utf-8")
+    result = sp.run(["./a.out"], stdout=sp.PIPE, text=True)
+    return result
 
 def test_print_H_72_c_option():
     sp.run(["rm", "a.out", "output.S"])
     code = """+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     +++++++."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 
 def test_print_H_72_stdin():
@@ -22,8 +24,8 @@ def test_print_H_72_stdin():
     sp.run(["rm", "a.out", "output.S"])
     sp.run(f"echo '{code}' | ./bf", shell=True)
     sp.run(["gcc", "check.c", "output.S"])
-    result = sp.run(["./a.out"], stdout=sp.PIPE)
-    assert result.stdout.decode("utf-8") == bf.evaluate(code)
+    result = sp.run(["./a.out"], stdout=sp.PIPE, text=True)
+    assert result.stdout == bf.evaluate(code)
 
 def test_print_H_72_file():
     code = """+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -31,39 +33,59 @@ def test_print_H_72_file():
     sp.run(["rm", "a.out", "output.S"])
     sp.run(["./bf", "print_H.bf"])
     sp.run(["gcc", "check.c", "output.S"])
-    result = sp.run(["./a.out"], stdout=sp.PIPE)
-    assert result.stdout.decode("utf-8") == bf.evaluate(code)
+    result = sp.run(["./a.out"], stdout=sp.PIPE, text=True)
+    assert result.stdout == bf.evaluate(code)
 
 def test_print_H_and_I_same_pointer():
     code = """+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     +++++++.+."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_print_H_and_I_different_pointer():
     code = """+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     +++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     +++++++."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_hello_world():
     code = """++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++
     .<<+++++++++++++++.>.+++.------.--------.>+.>."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_hello_world_print_end_only():
     code = """++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++>+++++++++++>++<<+++
     ++++++++++++>+++-------------->+>."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_simple_input():
     code = """,."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_back_to_zero():
     code = """,[-]."""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
 def test_empty():
     code = """"""
-    assert run_code(code) == bf.evaluate(code)
+    assert run_code(code).stdout == bf.evaluate(code)
 
+def test_sierpinski():
+    code = """++++++++[>+>++++<<-]>++>>+<[-[>>+<<-]+>>]>+[-<<<[->[+[-]+>++>>>-<
+    <]<[<]>>++++++[<<+++++>>-]+<<++.[-]<<]>.>+[>>]>+]"""
+    assert run_code(code).stdout == bf.evaluate(code)
+
+@pytest.mark.timeout(timeout)
+def test_big_array():
+    code = """++++[>++++++<-]>[>+++++>+++++++<<-]>>++++<[[>[[>>+<<-]<]>>>-]>-[>
+    +>+<<-]>]+++++[>+++++++<<++>-]>.<<."""
+    assert run_code(code).stdout == "#\n" # bf.evaluate(code) se fait timeout
+
+@pytest.mark.timeout(timeout)
+def test_unmatched_1():
+    code = """+++++[>+++++++>++<<-]>.>.["""
+    assert run_code(code).stdout == bf.evaluate(code)
+
+@pytest.mark.timeout(timeout)
+def test_unmatched_2():
+    code = """+++++[>+++++++>++<<-]>.>.]["""
+    assert run_code(code).stdout == bf.evaluate(code)
